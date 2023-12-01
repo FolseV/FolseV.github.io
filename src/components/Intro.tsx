d44b5119aa73ee1b6729238791f2215e5bb2e8f1
Folse <folsefeeder@gmail.com>
2023-12-01 17:33:13 +0300

    add intro info and update img

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -3,7 +3,11 @@ const Intro = () => {
     <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
       <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">Vlad Drevilo</h1>
       <p className="text-base md:text-xl mb-3 font-medium">Software Engineer & Web developer</p>
-      <p className="text-sm max-w-xl mb6 font-bold">This is my entire bio</p>
+      <p className="text-sm max-w-xl mb6 font-bold">
+        Hi! I'm Vlad and I am front-end web developer doing all things creative to make web a more unique and awesome place. Passionate
+        about learning and development with desire to apply skills on a larger development team. Eager to tackle more complex problems and
+        continue to find ways to maximize user efficiency
+      </p>
     </div>
   );
 };
