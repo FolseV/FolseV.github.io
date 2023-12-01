ce6fc6f4884d01dd45ee6ce169d54c82974d51f8
Folse <folsefeeder@gmail.com>
2023-12-01 16:23:35 +0300

    img fix

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -1,13 +1,13 @@
 export default [
   {
     title: "Minimal portfolio",
-    imgUrl: "/img/minimal-portfolio.png",
+    imgUrl: "public/img/minimal-portfolio.png",
     stack: ["Vite", "TS", "TailwindCSS", "React"],
     link: "https://github.com/FolseV/minimal-portfolio",
   },
   {
     title: "Memory game",
-    imgUrl: "/img/memory-game.jpg",
+    imgUrl: "public/img/memory-game.jpg",
     stack: ["React", "TypeScript", "Redux", "Redux-thunk", "React-router"],
     link: "https://github.com/FolseV/Memory-game",
   },
